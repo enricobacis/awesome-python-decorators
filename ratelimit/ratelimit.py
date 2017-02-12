e5a92ee8c44d07c9c40eb02ad646f34eb4b0c85b
Enrico Bacis <enrico.bacis@gmail.com>
2017-02-12 11:23:28 +0100

    ensure semaphore release in ratelimit

--- ratelimit/ratelimit.py
+++ ratelimit/ratelimit.py
@@ -17,11 +17,14 @@ def ratelimit(limit, every=1):
         @wraps(fn)
         def wrapper(*args, **kwargs):
             semaphore.acquire()
-            result = fn(*args, **kwargs)
-            timer = Timer(every, semaphore.release)
-            timer.setDaemon(True)      # allows the timer to be canceled on exit
-            timer.start()
-            return result
+            
+            try:
+                return fn(*args, **kwargs)
+            
+            finally:                   # ensure semaphore release
+                timer = Timer(every, semaphore.release)
+                timer.setDaemon(True)  # allows the timer to be canceled on exit
+                timer.start()
 
         return wrapper
 
